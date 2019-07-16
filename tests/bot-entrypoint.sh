1203dab4326061948b59cf3537e238cce8bcd75f
David Dworken <david@keyba.se>
2019-07-16 12:04:06 -0400

    Chaned filename to .keep to represent purpose

--- tests/bot-entrypoint.sh
+++ tests/bot-entrypoint.sh
@@ -4,7 +4,7 @@ IFS=$'\n\t'
 
 # For some reason it is necessary to touch a file in /mnt/ in order to get the volume permissions to work correctly
 # when keybaseca generate runs
-touch /mnt/a
+touch /mnt/.keep
 nohup bash -c "run_keybase -g &"
 sleep 3
 keybase oneshot --username $KEYBASE_USERNAME --paperkey "$PAPERKEY"
