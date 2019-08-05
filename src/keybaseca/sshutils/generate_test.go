8123dca64cbb687e34cf9966999c51cad94ac5bc
David Dworken <david@keyba.se>
2019-08-05 09:19:19 -0400

    Switch assert to require so tests fail immediately

--- src/keybaseca/sshutils/generate_test.go
+++ src/keybaseca/sshutils/generate_test.go
@@ -7,7 +7,6 @@ import (
 	"testing"
 
 	"github.com/keybase/bot-ssh-ca/src/shared"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -16,20 +15,20 @@ func TestGenerateNewSSHKey(t *testing.T) {
 	os.Remove(filename)
 
 	err := GenerateNewSSHKey(filename, false, false)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	err = GenerateNewSSHKey(filename, false, false)
-	assert.Errorf(t, err, "Refusing to overwrite existing key (try with --overwrite-existing-key or FORCE_WRITE=true if you're sure): "+filename)
+	require.Errorf(t, err, "Refusing to overwrite existing key (try with --overwrite-existing-key or FORCE_WRITE=true if you're sure): "+filename)
 
 	err = GenerateNewSSHKey(filename, true, false)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	bytes, err := ioutil.ReadFile(filename)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	require.True(t, strings.Contains(string(bytes), "PRIVATE"))
 
 	bytes, err = ioutil.ReadFile(shared.KeyPathToPubKey(filename))
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	require.False(t, strings.Contains(string(bytes), "PRIVATE"))
 	require.True(t, strings.HasPrefix(string(bytes), "ssh-"))
 }
