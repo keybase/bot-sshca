21cc950fc2c0581147112aa4f62f3acec79ae3e3
David Dworken <david@daviddworken.com>
2019-08-30 22:12:59 -0400

    Add temporary env debugging

--- docker/entrypoint-generate.sh
+++ docker/entrypoint-generate.sh
@@ -9,6 +9,7 @@ chown -R keybase:keybase /mnt
 sudo -i -u keybase bash << EOF
 source docker/env.sh
 export "FORCE_WRITE=$FORCE_WRITE"
+env
 nohup bash -c "run_keybase -g &"
 sleep 3
 keybase oneshot --username \$KEYBASE_USERNAME --paperkey "\$KEYBASE_PAPERKEY"
