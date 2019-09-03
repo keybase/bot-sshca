d8c786e6c6f39e02e2b0660ac97cef42e6899264
David Dworken <david@daviddworken.com>
2019-09-03 17:43:46 -0400

    Detach from docker-compose

--- integrationTest.sh
+++ integrationTest.sh
@@ -42,7 +42,7 @@ echo "Building containers..."
 cd ../docker/ && make && cd ../tests/
 docker-compose build
 echo "Running integration tests..."
-docker-compose up
+docker-compose up -d
 
 docker logs kssh -f | indent
 TEST_EXIT_CODE=`docker wait kssh`
