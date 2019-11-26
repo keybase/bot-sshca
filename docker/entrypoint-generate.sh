467b123b931af99cc85d9b34d8e74054a3a2a242
teutates <10206665+teutat3s@users.noreply.github.com>
2019-11-26 23:25:00 +0100

    remove unused kbfsfuse from generate script

--- docker/entrypoint-generate.sh
+++ docker/entrypoint-generate.sh
@@ -9,8 +9,6 @@ chown -R keybase:keybase /mnt
 sudo -i -u keybase bash << EOF
 source docker/env.sh
 export "FORCE_WRITE=$FORCE_WRITE"
-nohup bash -c "run_keybase -g 2>&1 | grep -v 'KBFS failed to FUSE mount' &"
-sleep 3
 keybase oneshot --username \$KEYBASE_USERNAME --paperkey "\$KEYBASE_PAPERKEY"
 bin/keybaseca generate
 EOF
