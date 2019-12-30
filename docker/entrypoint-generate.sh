4b27d2c13e4868b4910019e21c149f38bb9ff8d3
M Mou <mmou@keyba.se>
2019-12-30 15:18:16 -0800

    clean up teutat3s changes

--- docker/entrypoint-generate.sh
+++ docker/entrypoint-generate.sh
@@ -9,7 +9,7 @@ chown -R keybase:keybase /mnt
 sudo -i -u keybase bash << EOF
 source docker/env.sh
 export "FORCE_WRITE=$FORCE_WRITE"
-nohup bash -c "run_keybase -g 2>&1 | grep -v 'KBFS failed to FUSE mount' &"
+nohup bash -c "KEYBASE_RUN_MODE=prod kbfsfuse /keybase | grep -v 'ERROR Mounting the filesystem failed' &"
 sleep 3
 keybase oneshot --username \$KEYBASE_USERNAME --paperkey "\$KEYBASE_PAPERKEY"
 bin/keybaseca generate
