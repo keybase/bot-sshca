9d8ca06a771ec39ddd31e7ee294eb3509e71ae3b
Adrian Goins <oskapt@users.noreply.github.com>
2020-02-03 19:51:36 -0300

    fix issues with env vars and entrypoint (#79)
    * fix issues with env vars and entrypoint
    
    * update example env.list

--- docker/entrypoint-server.sh
+++ docker/entrypoint-server.sh
@@ -7,6 +7,9 @@ chown -R keybase:keybase /mnt
 
 # Run everything else as the keybase user
 sudo -i -u keybase bash << EOF
+export "TEAMS=$TEAMS"
+export "KEYBASE_USERNAME=$KEYBASE_USERNAME"
+export "KEYBASE_PAPERKEY=$KEYBASE_PAPERKEY"
 nohup bash -c "KEYBASE_RUN_MODE=prod kbfsfuse /keybase | grep -v 'ERROR Mounting the filesystem failed' &"
 sleep 3
 keybase oneshot
